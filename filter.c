ef7f2ee4297d4b4925fe09841824dbfb9a88930f
Jeff Klouda <jklouda1@nd.edu>
2017-04-19 17:45:10 -0400

    filter.c: complete

--- filter.c
+++ filter.c
@@ -10,6 +10,8 @@
 #include <fnmatch.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <libgen.h>
+#include <stdlib.h>
 
 /**
  * Test whether or not the given path should be filtered out of the output.
@@ -19,7 +21,75 @@
  * exclude, false to include).
  */
 bool        filter(const char *path, const Settings *settings) {
-    return false;
-}
+    bool returnVal = false;
+    struct stat buf;
+    if (lstat(path, &buf) < 0) {
+        fprintf(stderr, "%s", strerror(errno));
+        return true;
+    }
+    /* check -executable, -readable. -writable */
+    if (settings->access && access(path, settings->access) == 0)
+        return true;
+    
+    /* Empty files and directories*/
+    if (settings->empty) {
+        if (S_ISDIR(buf.st_mode)) {
+            if (!is_directory_empty(path))
+                return true;
+        }
+        if (S_ISREG(buf.st_mode)) {
+            if (buf.st_size != 0)
+                return true;
+        }
+    }
 
-/* vim: set sts=4 sw=4 ts=8 expandtab ft=c: */
+    /* Test file type f = 1, d = 2*/
+    if ((settings->type == 1) && !S_ISREG(buf.st_mode))
+        return true;
+    if ((settings->type == 2) && !S_ISDIR(buf.st_mode))
+        return true;
+    
+    /* Base of file matches shell pattern */
+    if (settings->name != NULL) {
+        char* tempPath = strdup(path);
+        char* bname = strdup(basename(tempPath));
+        free(tempPath);
+        if (fnmatch(settings->name, bname, 0)) {
+            free(bname);
+            return true;
+        }
+        free(bname);
+    }
+    
+    /* Path of file matches shell pattern */
+    if (settings->path != NULL) {
+        if (fnmatch(settings->path, path, 0))
+            return true;
+    }
+    /* check permissions */
+    int perm = buf.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO);
+    if (settings->perm) {
+        if (settings->perm != perm)
+            return true;
+    }
+
+    /* File was modified more recently than "newer" */
+    if (settings->newer) {
+        time_t mtime = get_mtime(path);
+        if (settings->newer > mtime)
+            return true;
+    }
+    
+    /* Check file's numeric user ID */
+    if (settings->uid >= 0) {
+        if (settings->uid != (int) buf.st_uid)
+            return true;
+    }
+
+    /* Check file's numeric group ID */
+    if (settings->gid >= 0) {
+        if (settings->gid != (int) buf.st_gid)
+            return true;
+    }
+
+}
